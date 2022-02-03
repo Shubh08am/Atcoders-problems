cc0b9b962e3bac986f3b070b34b9ab04412469ba
Shubham Yadav <shubh08am@gmail.com>
2022-02-03 19:01:26 +0530

    Create Not Overflow.cpp

---
+++ Not Overflow.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+long long t;
+cin>>t;
+
+//int m = 2147483647 ;
+//int n = -2147483648;
+
+if ( t>=INT_MIN && t<=INT_MAX)
+cout<<"Yes\n";
+
+else
+cout<<"No\n";
+
+return 0;
+}
